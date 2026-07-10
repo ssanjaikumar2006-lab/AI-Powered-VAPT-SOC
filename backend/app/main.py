9371c65a851f5b412b4483f137c550c42f67931b
ssanjaikumar2006-lab <ssanjaikumar2006@gmail.com>
2026-07-10 22:56:41 +0530

    Refactor backend to feature-based architecture

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from sqlalchemy import text
 
+#from app.db.database import engine
+from app.db.database import Base, engine
+from app.db import base
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="AI-Powered VAPT & SOC Platform",
     version="1.0.0",
-    description="Backend API for AI-Powered VAPT & SOC Platform"
+    description="Backend API"
 )
 
 
@@ -12,3 +18,19 @@ def root():
     return {
         "message": "AI-Powered VAPT & SOC Backend is running"
     }
+
+
+@app.get("/health")
+def health_check():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return {
+            "status": "Database Connected Successfully"
+        }
+
+    except Exception as e:
+        return {
+            "status": "Database Connection Failed",
+            "error": str(e)
+        }
